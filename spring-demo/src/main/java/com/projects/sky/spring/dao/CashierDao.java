f07687472438d57ab4cad32fa30a4daa1e064a46
zealot <2424115257@qq.com>
2016-04-09 16:40:57 +0800

    change

--- spring-demo/src/main/java/com/projects/sky/spring/dao/CashierDao.java
+++
@@ -1,8 +0,0 @@
-package com.projects.sky.spring.dao;
-
-import java.util.List;
-
-public interface CashierDao {
-
-	public void checkout(String username, List<String> isbns);
-}
--- spring-demo/src/main/java/com/projects/sky/spring/service/BookService.java
+++
@@ -1,6 +0,0 @@
-package com.projects.sky.spring.service;
-
-public interface BookService {
-
-	public void purchase(String username, String isbn);
-}
